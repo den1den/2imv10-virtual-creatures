27e362a9afd5105d8dab51f454caded52842b990
Mijael Ricardo Bueno Perez <mijaelbueno@gmail.com>
2016-02-17 15:39:30 +0100

    Setup Unity

---
+++ 2IMV10_VirtualCreatures/Assets/Scripts/Logic/Genotype.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Genotype : MonoBehaviour {
+
+    public float length = 5.0f;
+
+
+	public Genotype()
+    {
+    }
+
+
+    public void doSomething()
+    {
+        Genotype myGenotype = new Genotype();
+
+        myGenotype.length = 2.0f;
+    }
+}
