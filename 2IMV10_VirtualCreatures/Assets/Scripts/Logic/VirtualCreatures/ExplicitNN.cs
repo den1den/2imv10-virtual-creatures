5ebfd6f64570e69874e38216ffbeb63fccf610fb
Dennis van den Brand <dennis.vandenbrand@nubisonline.nl>
2016-02-18 16:24:34 +0100

    Ready for instantiating Unity objects

---
+++ 2IMV10_VirtualCreatures/Assets/Scripts/Logic/VirtualCreatures/ExplicitNN.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Assets.Scripts.Logic.VirtualCreatures
+{
+    internal class ExplicitNN
+    {
+        internal static ExplicitNN createNew(Morphology morphology)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ 2IMV10_VirtualCreatures/Assets/Scripts/Logic/VirtualCreatures/Phenotype.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+
+namespace Assets.Scripts.Logic.VirtualCreatures
+{
+    class Phenotype
+    {
+        Morphology morphology; //for tracking only
+        UnityEngine.Object someUnityOutput;
+        ExplicitNN theNetwork;
+
+        public Phenotype(Morphology morphology, UnityEngine.Object someUnityOutput, ExplicitNN theNetwork)
+        {
+            this.morphology = morphology;
+            this.someUnityOutput = someUnityOutput;
+            this.theNetwork = theNetwork;
+        }
+
+        public static Phenotype createNew(Morphology morphology)
+        {
+            //
+            UnityEngine.Object someUnityOutput = null;
+            throw new NotImplementedException();
+
+            //
+            ExplicitNN theNetwork = ExplicitNN.createNew(morphology);
+
+            return new Phenotype(morphology, someUnityOutput, theNetwork);
+        }
+    }
+}
